f2d667cb88957f2aa20ea354193570d20d215e94
Richard Herrmann <richard.herrmann@scout24.com>
2020-07-22 14:22:30 +0200

    first push

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # icm-list-appender
 Python tool that can append multiple icheckmovies.com lists to create "all editions" type movies lists. It can also trim lists to only include the first x entries of each list (given different versions of a single list), e.g. for creating lists like "top 10 of each year/user" (given different list submissions for a common subject).)
+
+Example uses:
+
+```python -m icm-list-appender -d yearbyyear -e 10```
+
+Have yearly top list files in yearbyyear directory, create an ascending list of all yearly top 10s
+
+```python -m icm-list-appender -d hiddengems -a```
+
+Have multiple editions of a Hidden Gems list in hiddengems directory,
+create 'all editions' list that shows current editions, extra movies for previous editions
+(and gives info about which list edition a movie is on)
---
+++ icm-list-appender.py
@@ -0,0 +1,62 @@
+import csv
+import glob
+import click as click
+
+
+def yield_csv_files_in_directory(directory: str, all_editions: bool):
+    for file in sorted(glob.glob(directory + "/*.csv"), reverse=all_editions):
+        icm_list = list()
+        with open(file, encoding='latin-1') as csvfile:
+            for entry in csv.reader(csvfile, delimiter=','):
+                icm_list.append(entry[11])
+            yield file[len(directory) + 1:-len("*.csv") + 1], icm_list[1:]
+
+
+class ICMListAppender:
+    def __init__(self, entries_per_list, all_editions):
+        self.list_starts = list()
+        self.imdb_urls = list()
+        self.entries_per_list = entries_per_list
+        self.all_editions = all_editions
+
+    def append_lists(self, name, icm_list):
+        list_start = len(self.imdb_urls) + 1
+
+        for i, entry in enumerate(icm_list):
+            if self.entries_per_list and i >= self.entries_per_list:
+                break
+
+            if entry not in self.imdb_urls:
+                self.imdb_urls.append(entry)
+
+        if self.all_editions:
+            self.list_starts.append((list_start, len(self.imdb_urls), name))
+
+    def print_results_to_file(self, directory):
+        with open(directory + '/results.txt', 'w') as file:
+            digits = '0' + str(len(str(self.list_starts[-1][1])))
+            for start, end, list_name in self.list_starts:
+                file.write(f"#{format(start, digits)}-#{format(end, digits)}: Last in {list_name}\n")
+            if self.list_starts:
+                file.write('\n\n\n')
+
+            for url in self.imdb_urls:
+                file.write(url + '\n')
+
+
+@click.command()
+@click.option("-d", "--directory", type=str, required=True, help='Directory name of the lists to be processed')
+@click.option("-a", "--all-editions", is_flag=True, required=False, help='Order the file names in ascending order ')
+@click.option("-e", "--entries-per-list", type=int, required=False, help='Maximum number of entries per list to be '
+                                                                         'processed')
+def go(directory, all_editions, entries_per_list):
+    if not entries_per_list:
+        entries_per_list = False
+    icm_list_appender = ICMListAppender(entries_per_list, all_editions)
+    for name, file in yield_csv_files_in_directory(directory, all_editions):
+        icm_list_appender.append_lists(name, file)
+    icm_list_appender.print_results_to_file(directory)
+
+
+if __name__ == '__main__':
+    go()
